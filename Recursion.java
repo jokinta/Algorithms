499e8286592493cccfd498c6fd71ed38ace553fe
Jovan Ivanov <36512938+jokinta@users.noreply.github.com>
2019-06-06 23:32:24 +0300

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -5,7 +5,7 @@ public class Recursion {
         System.out.println(Factoriel(5));
     }
     public static int Factoriel(int num){
-        if(num==0)
+        if(num<=0)
             return 1;
         else
             return num * Factoriel(num-1);
